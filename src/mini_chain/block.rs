cfede7c1e650a3176d0782d6f85c0a30f5e71f49
abharun <adambinharun1010@gmail.com>
2024-03-22 09:09:59 +0100

    implement transaction operation

--- src/mini_chain/block.rs
+++ src/mini_chain/block.rs
@@ -1,7 +1,7 @@
 use super::transaction::Transaction;
 
 pub struct Block {
-    pub timestamp: String,
+    pub timestamp: u64,
     pub transactions: Vec<Transaction>,
     pub nonce: u64,
     pub prev_hash: String,
--- src/simulator.rs
+++ src/simulator.rs
@@ -1,1 +1,3 @@
 pub fn chain_simulation(node_count: i32) {}
+
+pub fn client_simulation() {}
